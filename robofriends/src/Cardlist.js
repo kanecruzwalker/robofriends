939fecbeb9e54e98e7ed470de830ffc9bc04071b
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-28 16:19:04 -0700

    mapped over robots array, dynamically generating card components

--- robofriends/src/Cardlist.js
+++ robofriends/src/Cardlist.js
@@ -2,8 +2,14 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({robots}) => {
+    const cardComponent = robots.map((user, i) => {
+        return   <Card id ={robots[i].id } name = {robots[i].name} email = {robots[i].email}/>
+    })
+
     return(
-        <Card id ={robots[0].id } name = {robots[0].name} email = {robots[0].email}/>
+        <div>
+            {cardComponent}
+        </div>
     );
 }
 
