d535e9999a974e74efd216870c6426f8776f7012
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-28 15:50:28 -0700

    destructured props being pased before return

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 const Card = (props) => {
+    const { name , email, id } = props;
     return(
        <div>
-            <img src={`https://robohash.org/${props.id}`} alt="robots" />
+            <img src={`https://robohash.org/${id}`} alt="robots" />
         <div>
-            <h2>{props.name}</h2>
-            <p>{props.email}</p>
+            <h2>{name}</h2>
+            <p>{email}</p>
         </div>
        </div> 
     );
