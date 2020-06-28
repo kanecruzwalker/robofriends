bf7bd7d187e1b0239e87dc3f924111f80098362c
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-28 16:51:23 -0700

    added to app, and a component

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {Fragment} from "react";
 import CardList from "./Cardlist";
 import { robots } from "./robots";
+import SearchBox from "./Searchbox";
 
 const App = () => {
     return(
-        <CardList robots={robots}/>
-
+        <Fragment>
+            <h1>Welcome to Robot friends</h1>
+            <SearchBox/>
+            <CardList robots={robots}/>
+        </Fragment>
     )
 }
 
---
+++ robofriends/src/Searchbox.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from "react";
+
+const SearchBox = () => {
+    return(
+        <Fragment>
+            <h1>It's search time</h1>
+        </Fragment>
+    )
+}
+
+export default SearchBox
