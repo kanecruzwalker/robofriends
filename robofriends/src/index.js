8efb9f11f256127cfe6a97f5632730f76cdeb44a
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-28 16:38:18 -0700

    added App, to hold all components

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {robots} from './robots'
-import CardList from "./Cardlist"
+import App from "./App";
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-  <CardList robots={robots}/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
