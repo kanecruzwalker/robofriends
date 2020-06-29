d8dd213c576adebce875f2299f1f308dee712748
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-28 17:08:05 -0700

    added state and refactored to extend component

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,16 +1,23 @@
-import React, {Fragment} from "react";
+import React, {Fragment, Component} from "react";
 import CardList from "./Cardlist";
 import { robots } from "./robots";
 import SearchBox from "./Searchbox";
 
-const App = () => {
-    return(
+const state = {
+    robots: robots,
+    searchfield: ""
+}
+
+class App extends Component {
+    render(){
+        return(
         <Fragment>
             <h1>Welcome to Robot friends</h1>
             <SearchBox/>
             <CardList robots={robots}/>
         </Fragment>
-    )
+        )
+    }
 }
 
 export default App;
