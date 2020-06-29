49324d80056796b66b5e1ab95ed64b585927c598
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-28 22:42:23 -0700

    Updates

---
+++ precache-manifest.958a0d4cdd574aaa6460b7f5982c8618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d12850f22eb8013404bf35735255bf6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "cceee7a1455fb17fb3c0",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e13aa5aa07d8cdbcc425",
+    "url": "/robofriends/static/js/2.5e776654.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.5e776654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cceee7a1455fb17fb3c0",
+    "url": "/robofriends/static/js/main.d95434cc.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
