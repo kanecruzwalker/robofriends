d840a039d355deb9701ad1393be458499a2a3d29
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-29 19:15:38 -0700

    added new reducer and new state values to be passed around, using Redux, less react state, and redux-thunk to pass functions opposed to objects

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { createLogger } from "redux-logger";
+import  thunkMiddleware from "redux-thunk"
 import App from "./containers/App";
-import {searchRobots} from "./reducers"
+import {searchRobots, requestRobots} from "./reducers"
 
 import * as serviceWorker from './serviceWorker';
 
 const logger = createLogger();
 
-const store = createStore(searchRobots, applyMiddleware(logger));
+const rootReducer = combineReducers({ searchRobots, requestRobots})
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
   <React.StrictMode>
