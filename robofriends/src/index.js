d66f55bf42ab0d31b3c768f53d94f0fc4d3c5abb
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-29 18:22:23 -0700

    added logger middleware for working with redux and see what events are changing when and in what order

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
 import App from "./containers/App";
 import {searchRobots} from "./reducers"
 
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
